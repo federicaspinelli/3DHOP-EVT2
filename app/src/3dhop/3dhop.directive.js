610747ed8008742d6d8e7a382671e3f36622840b
federicaspinelli <federicaspinelli@msn.com>
2020-02-25 15:36:22 +0100

    Add function to extract contents of JSON file config3d

--- app/src/3dhop/3dhop.directive.js
+++ app/src/3dhop/3dhop.directive.js
@@ -1,6 +1,6 @@
 angular.module('evtviewer.3dhop')
 
-.directive('3dhop', function(evtAnalogue, evtInterface, $ocLazyLoad, $timeout, $scope) {
+.directive('3dhop', function(evtAnalogue, evtInterface, $ocLazyLoad, $timeout) {
 	return {
 		restrict: 'A',
 		scope: {
@@ -10,12 +10,18 @@ angular.module('evtviewer.3dhop')
 
 		link: function(scope, element, attrs) {
 
-         var presenter = null;
-
 			var pluginFolder = 'js-plugins/3dhop/';
-			var jsFiles = ['spidergl.js', 'jquery.js', 'presenter.js',
-				'nexus.js', 'ply.js', 'trackball_turntable.js',
-				'trackball_turntable_pan.js', 'trackball_pantilt.js', 'trackball_sphere.js', 'init.js'
+         var jsFiles = [
+         'spidergl.js',
+         'jquery.js',
+         'presenter.js',
+         'nexus.js',
+         'ply.js',
+         'trackball_turntable.js',
+         'trackball_turntable_pan.js',
+         'trackball_pantilt.js',
+         'trackball_sphere.js',
+         'init.js'
 			]
 
 			var loadFiles = function(fileIndex) {
@@ -28,22 +34,33 @@ angular.module('evtviewer.3dhop')
 				})
          }
 
-         var objurl = scope.url;
-
-			var initializeViewer = function() {
-				init3dhop();
-				setup3dhop();
-			};
-
-         loadFiles(0);
-
 			var setup3dhop = function() {
+            function readTextFile(file, callback) {
+               var rawFile = new XMLHttpRequest();
+               rawFile.overrideMimeType("application/json");
+               rawFile.open("GET", file, true);
+               rawFile.onreadystatechange = function() {
+                   if (rawFile.readyState === 4 && rawFile.status == "200") {
+                       callback(rawFile.responseText);
+                   }
+               }
+               rawFile.send(null);
+           }
+
+           //usage:
+           readTextFile("config/config3d.json", function(text){
+               var data = JSON.parse(text);
+               console.log("Caricamento JSON");
+               var myurl=data.meshes.url;
+               var myurl=data.meshes.url;
+               console.log(myurl);
+
 				presenter = new Presenter(scope.canvas);
 
             presenter.setScene({
 					meshes: {
-						"Bewcastle": {
-						   url: objurl
+						"Mesh": {
+						   url: myurl
 						},
 						//"Cage": {
 						//	url: "data/3Dmodels/singleres/"
@@ -52,14 +69,25 @@ angular.module('evtviewer.3dhop')
 					modelInstances: {
 
 						"Model1": {
-							mesh: "Bewcastle"
+							mesh: "Mesh"
 						},
 						//"Model2": {
 						//	mesh: ""
 						//}
 					}
-				});
-			}
+            });
+         });
+         }
+
+         var presenter = null;
+
+			var initializeViewer = function() {
+				init3dhop();
+				setup3dhop();
+			};
+
+         loadFiles(0);
+
 			scope.actionOnViewer = function(action) {
 				console.log('action');
 				try {
@@ -105,6 +133,6 @@ angular.module('evtviewer.3dhop')
 					console.log(e)
 				}
 			}
-		}
+      }
 	};
 });
